399083b8a16c34f87c90937256ff6678c1ab81f7
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2023-09-28 13:01:04 +0300

    TIntMask in IntMaskType should allow 0

--- src/IntMaskType.php
+++ src/IntMaskType.php
@@ -7,20 +7,20 @@ namespace Typhoon\Type;
 /**
  * @api
  * @psalm-immutable
- * @template-covariant TIntMask of positive-int
+ * @template-covariant TIntMask of int<0, max>
  * @implements Type<TIntMask>
  */
 final class IntMaskType implements Type
 {
     /**
-     * @var non-empty-list<positive-int>
+     * @var non-empty-list<int<0, max>>
      */
     public readonly array $ints;
 
     /**
      * @internal
      * @psalm-internal Typhoon\Type
-     * @param non-empty-list<positive-int> $ints
+     * @param non-empty-list<int<0, max>> $ints
      */
     public function __construct(
         array $ints,
