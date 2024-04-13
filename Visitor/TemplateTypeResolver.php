c6f2c3d04ce0fbc2c409b150ce3fb225418e3389
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-04-13 12:25:24 +0300

    Add TemplateTypeResolver

---
+++ Visitor/TemplateTypeResolver.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Type\Visitor;
+
+use Typhoon\Type\AtClass;
+use Typhoon\Type\AtFunction;
+use Typhoon\Type\AtMethod;
+use Typhoon\Type\Type;
+
+/**
+ * @api
+ * @readonly
+ */
+final class TemplateTypeResolver extends RecursiveTypeReplacer
+{
+    /**
+     * @var array<non-empty-string, Type>
+     */
+    private array $typesByHash = [];
+
+    /**
+     * @psalm-pure
+     * @param non-empty-string $name
+     * @return non-empty-string
+     */
+    private static function hash(string $name, AtClass|AtFunction|AtMethod $declaredAt): string
+    {
+        return $name . '.' . match (true) {
+            $declaredAt instanceof AtClass => $declaredAt->name,
+            $declaredAt instanceof AtFunction => $declaredAt->name,
+            $declaredAt instanceof AtMethod => $declaredAt->class . '.' . $declaredAt->name,
+        };
+    }
+
+    /**
+     * @param non-empty-string $name
+     */
+    public function with(string $name, AtClass|AtFunction|AtMethod $declaredAt, Type $type): self
+    {
+        $resolver = clone $this;
+        $resolver->typesByHash[self::hash($name, $declaredAt)] = $type;
+
+        return $resolver;
+    }
+
+    public function template(Type $self, string $name, AtClass|AtFunction|AtMethod $declaredAt, array $arguments): mixed
+    {
+        return $this->typesByHash[self::hash($name, $declaredAt)] ?? $self;
+    }
+}
