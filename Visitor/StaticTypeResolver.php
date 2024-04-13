dd75fbd0039d09102625ba7d731ce637db709bb5
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-04-13 11:42:34 +0300

    Add StaticTypeResolver

---
+++ Visitor/StaticTypeResolver.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Type\Visitor;
+
+use Typhoon\Type\RecursiveTypeReplacer;
+use Typhoon\Type\Type;
+use Typhoon\Type\types;
+
+/**
+ * @api
+ */
+final class StaticTypeResolver extends RecursiveTypeReplacer
+{
+    /**
+     * @param non-empty-string $class
+     */
+    public function __construct(
+        private readonly string $class,
+    ) {}
+
+    public function static(Type $self, string $class, array $arguments): mixed
+    {
+        return types::object($this->class, ...array_map(
+            fn(Type $templateArgument): Type => $templateArgument->accept($this),
+            $arguments,
+        ));
+    }
+}
