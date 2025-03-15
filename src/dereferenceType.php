3b797d3c8aa08f89e64789a774720a8abb20e791
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2025-03-15 11:40:41 +0300

    Add dereferenceType() function

---
+++ src/dereferenceType.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\Type;
+
+use Typhoon\Type\Visitor\RecursiveTypeReplacer;
+
+/**
+ * @api
+ */
+function dereferenceType(Type $type): Type
+{
+    /** @var ?RecursiveTypeReplacer */
+    static $dereferencer = null;
+    $dereferencer ??= new class extends RecursiveTypeReplacer {};
+
+    return $type->accept($dereferencer);
+}
